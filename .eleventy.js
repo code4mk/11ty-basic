7a21ad7a313ca19ed9b9e4fe4428189b32983236
mostafa <radionishi@gmail.com>
2021-05-15 21:17:13 +0600

    assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
     // Copy these static files to _site folder
-    eleventyConfig.addPassthroughCopy('assets')
+    eleventyConfig.addPassthroughCopy('src/assets')
     return {
     dir: { 
         input: 'src', 
