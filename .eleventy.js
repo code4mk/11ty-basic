71a9695671b032151ae1d1bbc599142d9ad959a6
mostafa <radionishi@gmail.com>
2021-05-15 20:39:11 +0600

    assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
+    // Copy these static files to _site folder
+    eleventyConfig.addPassthroughCopy('assets')
 };
--- tips.md
+++ tips.md
@@ -4,3 +4,7 @@
 * https://www.11ty.dev/docs/collections/
 
 * https://github.com/kohrongying/11ty-blog-starter/blob/master/src/_data/site.json
+
+
+* https://dev.to/t/11ty
+* https://11ty.rocks/
