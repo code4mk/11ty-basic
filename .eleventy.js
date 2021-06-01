57d4685eb8df5222044dbf53f54bbe45824b96a6
Mostafa Kamal <hiremostafa@gmail.com>
2021-06-01 18:04:32 +0600

    extensionless-html

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,52 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
     // Copy these static files to _site folder
     eleventyConfig.addPassthroughCopy('src/assets');
+
+  //   eleventyConfig.setBrowserSyncConfig({
+  //     middleware: [
+  //         function (req, res, next) {
+  //             if (/^[^.]+$/.test(req.url)) {
+  //                 res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  //             }
+  //             console.log("yo")
+  //             next();
+  //         }
+  //     ]
+  // });
+
+  const getLastItem = thePath => thePath.substring(thePath.lastIndexOf('/') + 1)
     
     eleventyConfig.setBrowserSyncConfig({
+      middleware: [
+                function (req, res, next) {
+                  let validatorRegex = /.html$/
+                  if(validatorRegex.exec(req.url) == null) {
+                    try {
+                      const content = fs.readFileSync('_site'+req.url.substring(0, req.url.length - 1)+'.html');
+                      res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+                      res.write(content);
+                      res.end();
+                    } catch (error) {
+                      let content
+                      if(req.url == '/'){
+                         content = fs.readFileSync('_site/index.html');
+                      }else{
+                         content = fs.readFileSync('_site/404.html');
+                      }
+                      res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+      
+                      res.write(content);
+                      res.end();
+                    }
+                  }else{
+                    res.writeHead(302, {
+                      'Location': req.url.substring(0, req.url.length - 5)
+                    });
+                    res.end();
+                    next()
+                  }   
+                }
+            ],
     callbacks: {
       ready: function(err, bs) {
 
